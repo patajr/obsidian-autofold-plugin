6d930b4d28edf1f6a77bde365e43e965ed95b517
deszksebas <patajr06@gmail.com>
2025-08-07 23:09:21 -0700

    fix

--- main.ts
+++ main.ts
@@ -1,41 +1,42 @@
 import { App, PluginSettingTab, Setting, Plugin, TFile } from 'obsidian';
 
-// INTERFAZ DE CONFIGURACIÓN
 interface AutoFoldSettings {
   autoFoldOnOpen: boolean;
 }
 
-// CONFIGURACIÓN POR DEFECTO
 const DEFAULT_SETTINGS: AutoFoldSettings = {
   autoFoldOnOpen: true,
 }
 
-// CLASE PRINCIPAL DEL PLUGIN
 export default class AutoFoldPlugin extends Plugin {
   settings: AutoFoldSettings;
-
+  private firstFileOpened = false;
   async onload() {
     await this.loadSettings();
     this.addSettingTab(new AutoFoldSettingTab(this.app, this));
-    console.log("Plugin de Plegado Automático cargado.");
+    // Fold al inicializar obsidian
+    this.app.workspace.onLayoutReady(() => {
+      (this.app as any).commands.executeCommandById('editor:fold-all');
+      });
 
+    // Opcion fold al abrir nota
     this.registerEvent(
       this.app.workspace.on('file-open', (file: TFile | null) => {
         if (file && this.settings.autoFoldOnOpen) {
-          console.log(`Plegando encabezados en: ${file.basename}`);
-          setTimeout(() => {
-          console.log(`Plegando encabezados en: ${file.basename}`);
+          if(!this.firstFileOpened){
+            (this.app as any).commands.executeCommandById('editor:fold-all');
+            this.firstFileOpened = true;
+          } else{
+            setTimeout(() => {
           (this.app as any).commands.executeCommandById('editor:fold-all');
-      }, 100); 
+            }, 100); 
+          }
+            
     }
   })
-    );
-  }
-
-  onunload() {
-    console.log("Plugin de Plegado Automático descargado.");
-  }
-
+);
+}
+  
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
   }
@@ -57,10 +58,9 @@ class AutoFoldSettingTab extends PluginSettingTab {
   display(): void {
     const { containerEl } = this;
     containerEl.empty();
-    containerEl.createEl('h2', { text: 'Auto Fold Settings' });
 
     new Setting(containerEl)
-      .setName('Automatically fold when a note is open') // Foldear automáticamente cuando una nota sea abierta
+      .setName('Automatically fold when a note is open')
       .setDesc('If on, every header will automatically fold whenever a note is open.')
       .addToggle(toggle => toggle
         .setValue(this.plugin.settings.autoFoldOnOpen)
